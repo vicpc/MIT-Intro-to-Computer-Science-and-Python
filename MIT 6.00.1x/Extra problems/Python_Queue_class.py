b4ec4a6ddfe76c58b8751562be2e1fe15d6128b2
vicpc <macbookpro@MacBook-Pro-de-Macbook-Pro.local>
2016-04-01 13:15:27 +0200

    Added Python Queue Class

---
+++ MIT 6.00.1x/Extra problems/Python_Queue_class.py
@@ -0,0 +1,21 @@
+class Queue(object):    
+    '''Initializes 1 attribute: a list to keep track of the queue's elements'''
+
+    def __init__(self):
+        '''Initializes 1 attribute: a list to keep track of the queue's elements'''
+        self.qlist = []
+        
+    def insert(self, element):
+        '''Uses the append method to add an element to the attribute list'''
+        self.qlist.append(element)
+                    
+    def remove(self):
+        '''Removes an element from the list'''
+        
+        #If list empty, raise ValueError
+        if self.qlist == []:
+            raise ValueError()
+        
+        #Pops an element from the front of the list, returns list
+        else:
+            return self.qlist.pop(0)
